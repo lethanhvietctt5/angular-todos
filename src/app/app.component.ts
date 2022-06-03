236e0894ae6c55b9c16b0f01d855a6799640ce88
lethanhvietctt5 <lethanhviet7c@gmail.com>
2022-06-03 17:53:58 +0700

    change input todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,12 @@ import {
   selector: 'app-root',
   template: ` <div class="w-full">
     <div class="w-1/3 mx-auto">
-      <p class="text-6xl text-center mt-6">Todo app</p>
+      <p class="text-6xl text-center mt-6">Todos app</p>
       <app-search-todo (searchTodo)="search($event)"></app-search-todo>
       <app-todo-list
         class="divide-y divide-slate-200"
-        [todos]="result"
+        [todos]="todos"
+        [query]="query"
         (deleteTodo)="delete($event)"
       ></app-todo-list>
       <app-create-todo (createTodo)="create($event)"></app-create-todo>
@@ -32,30 +33,17 @@ export class AppComponent {
     { id: 3, title: 'Learn JavaScript' },
   ];
   query = '';
-  result = this.todos.filter((todo) =>
-    todo.title.toLowerCase().includes(this.query.toLowerCase())
-  );
 
   delete(id: number) {
     this.todos = this.todos.filter((todo) => todo.id !== id);
-    this.result = this.todos.filter((todo) =>
-      todo.title.toLowerCase().includes(this.query.toLowerCase())
-    );
   }
 
   create(title: string) {
     const id = this.todos.length + 1;
     this.todos = [...this.todos, { id, title }];
-    this.result = this.todos.filter((todo) =>
-      todo.title.toLowerCase().includes(this.query.toLowerCase())
-    );
   }
 
   search(title: string) {
     this.query = title;
-    this.result = this.todos.filter((todo) =>
-      todo.title.toLowerCase().includes(this.query.toLowerCase())
-    );
   }
 }
-  
--- src/app/todo-list.component.ts
+++ src/app/todo-list.component.ts
@@ -1,20 +1,34 @@
 import { Todo } from './todo';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   template: `
     <app-todo-item
-      *ngFor="let todo of todos"
+      *ngFor="let todo of result"
       [todo]="todo"
       (deleteTodo)="delete($event)"
     ></app-todo-item>
   `,
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   @Input() todos!: Todo[];
   @Input() query!: string;
   @Output() deleteTodo = new EventEmitter<number>();
+
+  result: Todo[] = [];
+
+  ngOnInit(): void {
+    this.result = this.todos.filter((todo) => todo.title.includes(this.query));
+  }
 
   delete(id: number) {
     this.deleteTodo.emit(id);
